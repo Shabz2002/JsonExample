e7e9a6945e4ce396a3b7af7b2f0d8a51f5fd2b01
Shabz2002 <shahbazsheik75@gmail.com>
2025-04-29 22:50:25 +0300

    Initial commit - basic JSON deserialization

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace JsonExample
+{
+    class Program
+    {
+        public class User
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string City { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            string filePath = "user.json";
+
+            if (File.Exists(filePath))
+            {
+                string jsonResponse = File.ReadAllText(filePath);
+                User user = JsonConvert.DeserializeObject<User>(jsonResponse);
+
+                Console.WriteLine($"Name: {user.Name}");
+                Console.WriteLine($"Age: {user.Age}");
+                Console.WriteLine($"City: {user.City}");
+            }
+            else
+            {
+                Console.WriteLine("user.json not found!");
+            }
+        }
+    }
+}
