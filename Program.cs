3353661c3feea33b58d11a6607c8aa4d325c476a
Shabz2002 <shahbazsheik75@gmail.com>
2025-04-29 22:53:51 +0300

    Added support for multiple users with loop

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace JsonExample
 {
     class Program
     {
+        // Define a class to match the JSON structure
         public class User
         {
             public string Name { get; set; }
@@ -19,17 +21,28 @@ namespace JsonExample
 
             if (File.Exists(filePath))
             {
+                // Read JSON content from the file
                 string jsonResponse = File.ReadAllText(filePath);
-                User user = JsonConvert.DeserializeObject<User>(jsonResponse);
 
-                Console.WriteLine($"Name: {user.Name}");
-                Console.WriteLine($"Age: {user.Age}");
-                Console.WriteLine($"City: {user.City}");
+                // Deserialize the JSON into a list of User objects
+                List<User> users = JsonConvert.DeserializeObject<List<User>>(jsonResponse);
+
+                Console.WriteLine("User List:");
+                foreach (var user in users)
+                {
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine($"Name: {user.Name}");
+                    Console.WriteLine($"Age: {user.Age}");
+                    Console.WriteLine($"City: {user.City}");
+                }
             }
             else
             {
                 Console.WriteLine("user.json not found!");
             }
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
         }
     }
 }
